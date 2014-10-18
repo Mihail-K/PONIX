a12a8cfc6f528edcfe60c6cd430da97fe61da25d
Mihail-K <Mihail.K@fimu.tk>
2014-10-18 19:03:46 -0400

    Modified kernel to load in the higher half (above 3GB). Make script also now generates a disk image for testing.

--- Makefile
+++ Makefile
@@ -9,16 +9,23 @@ LDFLAGS = -T Linker.ld -nostdlib -O2 -L/usr/local/cross/lib/gcc/i686-elf/4.9.0
 CFLAGS = -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Werror
 CXXFLAGS = -std=gnu++11 -ffreestanding -O2 -Wall -Wextra -Werror -fno-exceptions -fno-rtti
 
+image = Kernel.iso
 objects = Start.o Kmain.o Console.o Utils.o GDT.o
 
-all: Kernel.bin
+all: $(image) Kernel.bin
 
 .PHONY: clean
 clean:
-	rm *.o *.bin
+	rm -rf *.o *.bin *.iso iso/
 
-Kernel.bin : $(objects)
-	$(LD) $(LDFLAGS) -o $@ $^ $(LDLIBS)
+Kernel.bin : $(objects) Linker.ld
+	$(LD) $(LDFLAGS) -o $@ $(objects) $(LDLIBS)
+
+$(image) : Kernel.bin
+	mkdir -p iso/boot/grub
+	cp Kernel.bin iso/boot/Kernel.bin
+	cp grub.cfg.template iso/boot/grub/grub.cfg
+	grub-mkrescue -o $@ iso/
 
 %.o : %.cxx *.hxx
 	$(CXX) $(CXXFLAGS) -c $< -o $@
--- Start.asm
+++ Start.asm
@@ -1,10 +1,16 @@
 
-.global Start
 .extern Kmain
 
 .set FLAGS, 1 + 2
 .set MAGIC, 0x1BADB002
 .set CHECKSUM, -(MAGIC + FLAGS)
+
+.set PRESENT, 1 << 0
+.set READWRITE, 1 << 1
+.set LARGEPAGE, 1 << 7
+.set ENTRYCOUNT, 1024
+
+.set VOFFSET, 0xC0000000
 
 .section .mboot
 .align 4
@@ -14,25 +20,63 @@ MbootHeader:
 	.long FLAGS
 	.long CHECKSUM
 
-.section .text
+.section .data
+.align 4096
 
+.global PageDirectory
+PageDirectory:
+	.long PRESENT | READWRITE | LARGEPAGE
+	.fill 768 - 1, 4, 0
+	.long PRESENT | READWRITE | LARGEPAGE
+	.fill 1024 - 786, 4, 0
+
+.section .textlow
+.align 4
+
+.global StartLow
+.type StartLow, @function
+StartLow:
+	cli
+
+	movl $(PageDirectory - VOFFSET), %ecx
+	movl %ecx, %cr3
+
+	movl %cr4, %ecx
+	orl $0x10, %ecx
+	movl %ecx, %cr4
+
+	movl %cr0, %ecx
+	orl $0x80000000, %ecx
+	movl %ecx, %cr0
+
+	leal (Start), %ecx
+	jmp *%ecx
+
+.section .text
+.align 4
+
+.type Start, @function
 Start:
 	movl $StackStart, %esp
 
+	pushl $0
+	popf
+
+	addl $VOFFSET, %ebx
 	pushl %ebx
 	pushl %eax
 
 	call Kmain
-	cli
 
 KernelHang:
 	hlt
+	cli
 	jmp KernelHang
 
 .section .bss
 .align 32
 
 StackEnd:
-	.space 0x1000
+	.fill 4096
 StackStart:
 
